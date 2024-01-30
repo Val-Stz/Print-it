ac6fef019220ca288960ef81c257c7e9097a940c
DwayneRS <valentin.stazewski@hotmail.fr>
2024-01-30 14:27:20 +0100

    Feat: JS Create Function updateSlide

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,43 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    {
+        image: "slide1.jpg",
+        tagLine:
+            "Impressions tous formats <span>en boutique et en ligne</span>",
+    },
+    {
+        image: "slide2.jpg",
+        tagLine:
+            "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+    },
+    {
+        image: "slide3.jpg",
+        tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+    },
+    {
+        image: "slide4.png",
+        tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+    },
+];
+
+let currentIndex = 0;
+const prevImage = document.getElementById("prevImg");
+const nextImage = document.getElementById("nextImg");
+const bannerImg = document.querySelector(".banner-img");
+const path = "./assets/images/slideshow/";
+const tagLine = document.querySelector("#banner p");
+
+function updateSlide(index) {
+    const currentSlide = slides[index];
+    bannerImg.src = path + currentSlide.image;
+    tagLine.innerHTML = currentSlide.tagLine;
+}
+
+prevImage.addEventListener("click", function () {
+    currentIndex--;
+    updateSlide(currentIndex);
+});
+
+nextImage.addEventListener("click", function () {
+    currentIndex++;
+    updateSlide(currentIndex);
+});
